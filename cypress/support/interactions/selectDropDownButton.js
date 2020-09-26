7a220d0707d3522d7e9db63ad627cb3263980953
Meneses <cesar.daniel.meneses@accenture.com>
2020-09-26 09:49:49 -0500

    se modifica la interaccion selectDropDownButton.js

--- cypress/support/interactions/selectDropDownButton.js
+++ cypress/support/interactions/selectDropDownButton.js
@@ -1,13 +1,15 @@
 const newRepositoryPage = require('../ui/createNewRepositoryPage');
 
 Cypress.Commands.add("selectGitignore", (filterValue) => {
+    cy.get(newRepositoryPage.CHECKBOX_GITIGNORE).click();
     cy.get(newRepositoryPage.ADD_GITIGNORE).click();
     cy.get(newRepositoryPage.FILTER_GITIGNORE).type(filterValue);
     cy.get(newRepositoryPage.GITIGNORE_OPTION_TO_SELECT).click();
 });
 
 Cypress.Commands.add("selectLicense", (filterValue) => {
-    cy.get(newRepositoryPage.ADD_LICENSE).click()
+    cy.get(newRepositoryPage.CHECKBOX_LICENSE).click();
+    cy.contains(newRepositoryPage.ADD_LICENSE).click()
     cy.get(newRepositoryPage.FILTER_LICENSE).type(filterValue);
     cy.xpath(newRepositoryPage.LICENSE_OPTION_TO_SELECT(filterValue.toLowerCase()))
         .click({ force: true });
